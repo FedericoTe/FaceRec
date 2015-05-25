c512113606366cc7b24db1dbd1b52c1abc932bd7
Federico <ftejeiro@gmail.com>
2015-05-25 18:02:55 +0200

    Codigo.R Lee una imagen e identifica los puntos faciales

--- Codigo.R
+++ Codigo.R
@@ -14,15 +14,30 @@ setwd("~/Personal/GITHUB//FaceRec/")
 
 # Cargar los dos data frame 
 # Para las imagenes solo, comentar las dos lineas siguientes
-Train <- read.csv("../FaceRep_Manual/Data//training.csv",stringsAsFactors=F))
+Train <- read.csv("../DATA_FaceRec//Data/training.csv",stringsAsFactors=F)
 Test <- read.csv("./Data//test.csv")
 
 # 
 # La ultima columna de Train es una imagen, la grabamos aparte, separando cada texto en números enteros
-# Para grabar solo las imagenes descometnar la siguiente linea
-# Train_Images <- read.csv("../FaceRep_Manual/Data//Imagenes.RData",stringsAsFactors=F))
-Imagenes_Train <- as.integer(unlist(strsplit(Train$Image, " ")))
+#
 
-m <- matrix(Train_Images[5],96,96)
+Imagenes_Train_1 <- as.integer(unlist(strsplit(Train$Image[1], " ")))
 
-image(m)
+# Para convertirlas en imagenes debemos crear una matriz de 96 * 96 y previamente tenemos que revertir la matriz, poner el primer número al final, el segundo el penúltimo,... y
+
+m <- matrix(rev(Imagenes_Train_1),96,96)
+
+# Para ver las imagenes una a una  la ponemos en blanco y negro
+
+image(1:96,1:96,m,col=gray((0:255)/255))
+
+# Podemos ahora marcar los puntos de esta imagen con:
+
+for (i in 1:(ncol(Train)-2)){
+  points(96-Train[1,i],96-Train[1,i+1], col = "blue", pch = 19)
+  i <- i+1
+}
+
+
+
+

6d95c2f4e5143e3df176d6ca1525cfab8765af23
Federico <ftejeiro@gmail.com>
2015-06-04 13:21:41 +0200

    Identificacion del pixel

--- Parches.R
+++ Parches.R
@@ -21,10 +21,10 @@ Todas_Imagenes <- readRDS("DATA/Imagenes.saved")
 
 # Seleccionamos el parche dando los valores X e Y de la esquina superior derecha y los dos desplazamientos Delta_X y Delta_Y
 
-X_Origen <- 15
+X_Origen <- 25
 Y_Origen <- 45
 X_Delta <- 30
-Y_Delta <- 30
+Y_Delta <- 20
 
 # Seleccionamos una imagen de las 7049 imagenes totales
 
@@ -37,3 +37,14 @@ parche <- Todas_Imagenes[[Seleccion_Imagen]][X_Origen:(X_Origen+X_Delta),Y_Orige
 # Podemos ver el parche con:
 
 image(X_Origen:(X_Origen+X_Delta),Y_Origen:(Y_Origen+Y_Delta),parche,col=gray((0:255)/255))
+
+# Podemos marcar el punto de esa imagen sabiendo la columna, en este caso las coordenadas del ojo iquierdo, extraídas con colnames(Train) mediante la función point:
+
+X_Punto <-  1  # left_eye_center_x
+Y_Punto <- 2  # left_eye_center_y
+
+points(((96-Train[Seleccion_Imagen,X_Punto])-X_Origen+1),((96-Train[Seleccion_Imagen,Y_Punto])-Y_Origen+1), col="blue",pch=19)
+
+# y la intensidad de ese punto es:
+
+parche[((96-Train[Seleccion_Imagen,X_Punto])-X_Origen+1),((96-Train[Seleccion_Imagen,Y_Punto])-Y_Origen+1)]
